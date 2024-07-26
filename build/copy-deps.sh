9dadf98f594069f43bea16266c3d17e65a7ad5ef
Ksenia Vazhdaeva <vazhdaeva_ks@bw-sw.com>
2024-07-26 13:35:39 +0700

    Use different names for client/server binaries in Docker images

--- build/copy-deps.sh
+++ build/copy-deps.sh
@@ -7,4 +7,5 @@ RUST_STD_LIB=$(find / -name 'libstd-*.so' | head -n1)
 echo "Rust std lib: $RUST_STD_LIB"
 cp $RUST_STD_LIB /opt/libs/
 cp target/release/deps/libsavant_core-*.so /opt/libs/
-cp "target/release/media_gateway_$1" /opt/bin/
+cp "target/release/media_gateway_client" /opt/bin/
+cp "target/release/media_gateway_server" /opt/bin/
